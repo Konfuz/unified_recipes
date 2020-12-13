e2bd582b5cf26329588cccee4bf58184d2a30987
Konfuz <timon@bit-bots.de>
2020-12-13 20:43:51 +0100

    white dye now craftable from chalk_powder closes #15

--- chalk.lua
+++ chalk.lua
@@ -1,5 +1,7 @@
 if minetest.registered_nodes["darkage:chalk"] and
 minetest.registered_items["darkage:chalk_powder"] then
+
+  -- allow making chalk blocks from the powder again
   minetest.register_craft({
     type = "shapeless",
     output = "darkage:chalk",
@@ -7,4 +9,13 @@ minetest.registered_items["darkage:chalk_powder"] then
       "darkage:chalk_powder", "darkage:chalk_powder"
     },
   })
+
+  -- allow making white dye from chalk_powder
+  if minetest.registered_items["dye:white"] then
+    minetest.register_craft({
+      type = 'shapeless',
+      output = 'dye:white',
+      recipe = {"darkage:chalk_powder"}
+    })
+  end
 end
